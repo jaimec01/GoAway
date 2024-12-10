57fcdf0dfef122099aeea1647480ebd73ca6bf84
jaime <jaime.castro@estudiantes.uva.es>
2024-12-10 18:56:13 +0100

    Agregar front como carpeta regular

---
+++ back/src/main/java/com/tfg/GoAway/GoAwayApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.GoAway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoAwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoAwayApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/tfg/GoAway/GoAwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.GoAway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoAwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
